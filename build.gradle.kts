b929fec633d5178cc03a937de14f2ea078be0258
Carlos <echoulen@gmail.com>
2022-09-18 16:18:47 +0800

    chore: resolve #1 install nut js and add guide docs (#13)
    * chore: install nut js
    
    * docs: add OpenCV Installation
    
    * docs: add node and npm version
    
    * done
    
    * revert

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,20 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-nodejs:0.0.7")
+    implementation(npm("@nut-tree/nut-js", "next"))
+    implementation(npm("@nut-tree/template-matcher", "next"))
+    implementation(npm("opencv4nodejs-prebuilt", "5.3.3"))
 }
 
 kotlin {
     js(IR) {
         binaries.executable()
-        nodejs {
-            
-        }
+        nodejs {}
+    }
+}
+
+plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+        nodeVersion = "15.11.0"
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,13 @@
+import external.nuttree.LoadMatcher
+import external.nuttree.Screen
+import external.nuttree.imageResource
+
 fun main() {
     println(greeting("ology-kt"))
+    LoadMatcher
+    Screen.find(imageResource("kotlin/template/act4wp.png"), null)
+        .then { println(it) }
+        .catch { println("can't find act4wp") }
 }
 
 fun greeting(name: String) =
