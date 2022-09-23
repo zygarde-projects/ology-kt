5302c8041e1a6d1d996c5d0832ee1f2fb64be4a9
Carlos <echoulen@gmail.com>
2022-09-23 19:28:43 +0800

    feat: resolve #5 check is running D2R or not (#21)

---
+++ src/main/kotlin/external/wincontrol/win-control.kt
@@ -0,0 +1,13 @@
+package external.wincontrol
+
+data class Dimensions(
+    val left: Int,
+    val right: Int,
+    val top: Int,
+    val bottom: Int
+)
+
+external class WindowInstance {
+    fun setForeground()
+    fun getDimensions(): Dimensions
+}
