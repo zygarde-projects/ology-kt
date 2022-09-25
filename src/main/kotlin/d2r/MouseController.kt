b844ae1f18575abddc9c25ec67165e62348e2b44
Carlos <echoulen@gmail.com>
2022-09-25 16:34:41 +0800

    feat: impl clickRelativeXY of MouseController (#26)

--- src/main/kotlin/d2r/MouseController.kt
+++ src/main/kotlin/d2r/MouseController.kt
@@ -1,14 +1,30 @@
 package d2r
 
 import extension.await
+import extension.toPoint
 import external.nuttree.Region
 import external.nuttree.centerOf
 import external.nuttree.mouse
 import external.nuttree.straightTo
+import external.wincontrol.WinControl.Window
 
 object MouseController {
+    private const val gameWindowTitle = "Diablo II: Resurrected"
+
     suspend fun clickOnRegionCenter(region: Region) {
         mouse.move(straightTo(centerOf(region))).await()
         mouse.leftClick().await()
+    }
+
+    /**
+     * base screen 3840 x 2160
+     */
+    suspend fun clickRelativeXY(x: Int, y: Int) {
+        Window.getByTitle(gameWindowTitle)?.getDimensions()
+            .takeIf { it != null }
+            .run {
+                val point = this!!.toPoint(x, y, baseScreenW, baseScreenH)
+                mouse.move(arrayOf(point)).await()
+            }
     }
 }
