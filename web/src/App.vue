d9beb5ecaa7485bec5bdc8fde76c322ca525727b
carlos <echoulen@gmail.com>
2022-09-28 14:12:01 +0800

    feat: init web

---
+++ web/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="app">
+    <a href="https://github.com/zygarde-projects/ology-kt" target="_blank">
+      <img src="/diablo2-logo-lg.png" class="logo" alt="ology kt" />
+    </a>
+  </div>
+  <HelloWorld msg="ology kt" />
+</template>
+
+<style scoped>
+.logo {
+  max-height: 30em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ web/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import axios from "axios";
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const ip = ref("localhost")
+
+const makeGame = async () => {
+    await axios.get("http://localhost:16668/ng")
+}
+
+</script>
+
+<template>
+  <h1>{{ ip }}:16668</h1>
+
+    <input v-model="ip"/>
+
+  <div class="card">
+    <button type="button" @click="makeGame">current game is foo</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
