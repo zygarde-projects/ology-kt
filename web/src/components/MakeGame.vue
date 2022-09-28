7f192b2b74b08ce0e3e6eec75e73ee3d84791558
carlos <echoulen@gmail.com>
2022-09-28 14:27:04 +0800

    feat: display game name and passord

--- web/src/components/MakeGame.vue
+++ web/src/components/MakeGame.vue
@@ -5,9 +5,11 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const ip = ref("localhost")
+let game = ref("unknown")
 
 const makeGame = async () => {
-    await axios.get("http://localhost:16668/ng")
+    const res = await axios.get("http://localhost:16668/ng")
+    game = res.data
 }
 
 </script>
@@ -18,9 +20,14 @@ const makeGame = async () => {
     <input v-model="ip"/>
 
   <div class="card">
-    <button type="button" @click="makeGame">current game is foo</button>
+    <button type="button" @click="makeGame">current game is {{ game }}</button>
   </div>
 </template>
 
 <style scoped>
+input, button {
+    font-size: 1.2em;
+    line-height: 3em;
+    border-radius: 1em;
+}
 </style>
