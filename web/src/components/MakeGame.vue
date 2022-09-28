ef65823c60e48841803775f8f9ed238b3ae61e56
Carlos <echoulen@gmail.com>
2022-09-28 17:26:12 +0800

    feat: init web (#40)

---
+++ web/src/components/MakeGame.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import axios from "axios";
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const ip = ref("localhost")
+let game = ref("unknown")
+
+const makeGame = async () => {
+    const res = await axios.get("http://localhost:16668/ng")
+    game = res.data
+}
+
+</script>
+
+<template>
+  <h1>{{ ip }}:16668</h1>
+
+    <input v-model="ip"/>
+
+  <div class="card">
+    <button type="button" @click="makeGame">current game is {{ game }}</button>
+  </div>
+</template>
+
+<style scoped>
+input, button {
+    font-size: 1.2em;
+    line-height: 3em;
+    border-radius: 1em;
+}
+</style>
