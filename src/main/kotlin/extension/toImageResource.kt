c74a9e574ac1e66e05cc86bf75e6f6fc17f0379f
Leo <leolin310148@gmail.com>
2022-09-29 19:04:00 +0800

    fix: image resource path

--- src/main/kotlin/extension/toImageResource.kt
+++ src/main/kotlin/extension/toImageResource.kt
@@ -1,7 +1,8 @@
 package extension
 
+import __dirname
 import external.nuttree.imageResource
 
 typealias TemplateImage = String
 
-fun TemplateImage.toImageResource() = imageResource("resources/template/$this")
+fun TemplateImage.toImageResource() = imageResource("$__dirname/resources/template/$this")
