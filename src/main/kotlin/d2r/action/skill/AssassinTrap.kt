495f025896d82e1df3aa47f8e414a239138f0b72
Leo <leolin310148@gmail.com>
2022-10-08 22:56:13 +0800

    feat: make trap cast faster (should able to config in future)

--- src/main/kotlin/d2r/action/skill/AssassinTrap.kt
+++ src/main/kotlin/d2r/action/skill/AssassinTrap.kt
@@ -4,16 +4,16 @@ import conf.SkillConfig
 import d2r.action.base.RepeatableSkillCastAction
 
 object AssassinTrap : RepeatableSkillCastAction() {
-  override val castInterval: Int = 12000
+  override val castInterval: Int = 10000
 
   private val key1 = SkillConfig.getKeyBtn("Trap1")
   private val key2 = SkillConfig.getKeyBtn("Trap2")
 
   override suspend fun doCast() {
-    cast(key1, 1000, 300)
-    cast(key1, 1000, 300)
-    cast(key1, 1000, 300)
-    cast(key1, 1000, 300)
-    cast(key2, 1000, 300)
+    cast(key1, 600, 300)
+    cast(key1, 600, 300)
+    cast(key1, 600, 300)
+    cast(key1, 600, 300)
+    cast(key2, 600, 300)
   }
 }
