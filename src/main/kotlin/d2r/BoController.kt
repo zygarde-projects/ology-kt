7b4da19b8e2a6ec3a37276ebcb6521704163520d
carlos <echoulen@gmail.com>
2022-10-12 18:39:58 +0800

    chore: add log for bo man flow 2

--- src/main/kotlin/d2r/BoController.kt
+++ src/main/kotlin/d2r/BoController.kt
@@ -12,18 +12,18 @@ import extension.waitRandomly
 import kotlinx.coroutines.delay
 import timers.clearInterval
 import timers.setInterval
+import kotlin.js.Date
 
 object BoController {
   private var boIntervalId: Timeout? = null
 
   suspend fun start() {
     ScreenController.oneOfImagesIn(ImageMatching.act4tp)?.region?.run {
-      MouseController.clickOnRegionCenter(this).wait(5000)
+      MouseController.clickOnRegionCenter(this).log("click on act4tpCenter ${Date.now()}")
+      delay(5000)
       ScreenController.oneOfImagesIn(ImageMatching.wpMenuTabAct4)?.region?.run {
-        log("found wpMenuTabAct4 ")
         delay(2000)
-        log("go to fireRiver ")
-        MouseController.clickOn(fireRiver).wait(3000)
+        MouseController.clickOn(fireRiver).log("click on fireRiver ${Date.now()}").wait(3000)
         when (ScreenController.oneOfImagesIn(ImageMatching.fireRiver)) {
           null -> log("fireRiverMark not detected, bo won't start")
           else -> {
@@ -31,8 +31,8 @@ object BoController {
             boIntervalId = setInterval({ launch { run() } }, 2000)
           }
         }
-      }.takeIf { it == null }.run { log("not found wpMenuTabAct4") }
-    }.takeIf { it == null }.run { log("not found act4tp") }
+      }
+    }
   }
 
   fun stop() = boIntervalId?.run {
